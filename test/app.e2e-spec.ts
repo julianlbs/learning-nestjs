4ffd2fc0c8bff6d9c35868be0bb78e89487909bf
julianlbs <julianlimalbs@gmail.com>
2022-11-03 15:48:41 -0300

    test: create e2e sign-up and sign-in tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
+import * as pactum from "pactum";
 import { PrismaService } from "../src/prisma/prisma.service";
 import { AppModule } from "../src/app.module";
+import { AuthDto } from "../src/auth/dto";
 
 describe("App e2e", () => {
   let app: INestApplication;
@@ -15,9 +17,11 @@ describe("App e2e", () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+    pactum.request.setBaseUrl("http://localhost:3333");
   });
 
   afterAll(() => {
@@ -25,11 +29,31 @@ describe("App e2e", () => {
   });
 
   describe("Auth", () => {
+    const dto: AuthDto = {
+      email: "julianlimalbs@gmail.com",
+      password: "hello123",
+    };
     describe("Signup", () => {
-      it.todo("should signup");
+      it("should sign-up", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody(dto)
+          .expectStatus(201)
+          .inspect();
+      });
     });
 
-    describe("Signin", () => {});
+    describe("Signin", () => {
+      it("should sign-in", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody(dto)
+          .expectStatus(200)
+          .inspect();
+      });
+    });
   });
 
   describe("User", () => {
